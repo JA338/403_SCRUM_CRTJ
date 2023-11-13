13b346e3e3889d8ec8569dcf899415450f586935
ryanerbelding <77171786+ryanerbelding@users.noreply.github.com>
2023-11-13 15:12:18 -0600

    readded lost stuff from merge

---
+++ Project/Fall2020_CSC403_Project/SaveGame.cs
@@ -0,0 +1,32 @@
+﻿using Fall2020_CSC403_Project.code;
+using Fall2020_CSC403_Project;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Fall2020_CSC403_Project.code
+{
+    public class SaveGame
+    {
+        public Player player { get; set; }
+        //public SaveGame() { }
+        public static void Save(Player player)
+        {
+            string docPath = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            using (StreamWriter saveFile = new StreamWriter(Path.Combine(docPath, "SaveFile.txt")))
+            {
+                Console.WriteLine(docPath);
+                saveFile.WriteLine(Game.levelData);
+                saveFile.WriteLine(player.Health);
+                saveFile.WriteLine(player.Position.x);
+                saveFile.WriteLine(player.Position.y);
+                saveFile.WriteLine(Game.scoreData);
+            }
+        }
+
+    }
+}
--- Project/Fall2020_CSC403_Project/Title.cs
+++ Project/Fall2020_CSC403_Project/Title.cs
@@ -6,7 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using Fall2020_CSC403_Project.code;
 
 namespace Fall2020_CSC403_Project
 {
@@ -37,9 +39,9 @@ namespace Fall2020_CSC403_Project
         private void button2_Click(object sender, EventArgs e)
         {
             this.Hide();
-            
+
             var frmLevel = new FrmLevelForest();
-            
+
             frmLevel.Closed += (s, args) => this.Close();
             frmLevel.Show();
         }
@@ -48,7 +50,7 @@ namespace Fall2020_CSC403_Project
         {
             this.Hide();
             var frmLevel = new FrmLevelGatefront();
-            
+
             frmLevel.Closed += (s, args) => this.Close();
             frmLevel.Show();
         }
@@ -59,6 +61,48 @@ namespace Fall2020_CSC403_Project
             var frmLevel = new FrmLevelCastle();
             frmLevel.Closed += (s, args) => this.Close();
             frmLevel.Show();
+        }
+
+        private void loadButton_Click_1(object sender, EventArgs e)
+        {
+            using (var sr = new StreamReader("SaveFile.txt"))
+            {
+                string levelID = sr.ReadLine();
+                Console.WriteLine(levelID);
+                if (levelID == "Level 1")
+                {
+                    this.Hide();
+                    //var frmLevel = new FrmLevelGatefront();
+                    var frmLevel = new FrmLevelForest();
+                    //var frmLevel = new FrmLevelCastle();
+                    frmLevel.Closed += (s, args) => this.Close();
+                    frmLevel.Show();
+                }
+
+                if (levelID == "Level 2")
+                {
+                    this.Hide();
+                    var frmLevel = new FrmLevelGatefront();
+                    //var frmLevel = new FrmLevelForest();
+                    //var frmLevel = new FrmLevelCastle();
+                    frmLevel.Closed += (s, args) => this.Close();
+                    frmLevel.Show();
+                }
+
+                if (levelID == "Level 3")
+                {
+                    this.Hide();
+                    //var frmLevel = new FrmLevelGatefront();
+                    //var frmLevel = new FrmLevelForest();
+                    var frmLevel = new FrmLevelCastle();
+                    frmLevel.Closed += (s, args) => this.Close();
+                    frmLevel.Show();
+                }
+                //sr.ReadLine();
+                Game.player.Health = Int32.Parse(sr.ReadLine());
+                Game.player.Position = new Vector2(Int32.Parse(sr.ReadLine()), Int32.Parse(sr.ReadLine()));
+                Game.scoreData = Int32.Parse(sr.ReadLine());
+            }
         }
     }
 }
